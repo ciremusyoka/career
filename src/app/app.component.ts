92eed503cf91e11e002dd64341074fd558a706a4
ciremusyoka <wambua.eric01@gmail.com>
2018-09-01 15:22:13 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'career-app';
+  photoa = '';
+  photob = '';
+  headimg = '';
+
+  constructor() {
+    this.photoa = 'assets/img/career1.jpeg';
+    this.photob = 'assets/img/career2.jpeg';
+    this.headimg = 'assets/img/bg.jpeg';
+   }
+
+  ngOnInit() {
+  }
 }
